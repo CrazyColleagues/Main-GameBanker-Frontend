adc06d063a9e24c8efebf7f891183209bcb39b8d
yogaardiansyah <yogaardiansyah@protonmail.com>
2024-11-22 19:36:14 +0700

    feat : tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default{
+  content: ["./src/**/*.{html,tsx,ts}"], //you can also add jsx and any other file types that you need to style with tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
